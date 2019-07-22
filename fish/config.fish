a69206f9439b0c82025a02b7c09f992401122bbf
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2019-07-22 13:07:17 +0900

    modify meme

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,10 @@ set -x BINARYDIR $GITDIR/binaries
 ### GITDIR
 set -x PATH $PATH $GITDIR/Cgaln $GITDIR/DROMPA3 $GITDIR/DROMPA3/scripts $GITDIR/DROMPAplus/bin $GITDIR/DROMPAplus/cpdf/Linux-Intel-64bit $GITDIR/SSP/bin $GITDIR/ChIPseqTools/bin $GITDIR/script_rnakato $GITDIR/script_Hi-C
 set -x PATH $PATH $GITDIR/RSEM $GITDIR/STAR/bin/Linux_x86_64_static $GITDIR/bedtools2/bin $GITDIR/bamtools/bin $GITDIR/salmon/bin $GITDIR/HOMER/bin $GITDIR/UCSC_utils/ $GITDIR/BaMMmotif2/build/bin
-set -x PATH $PATH $GITDIR/UCSC_utils $GITDIR/motif/meme_4.12.0/build/bin
+set -x PATH $PATH $GITDIR/UCSC_utils $GITDIR/motif/meme-5.0.5/build/bin
 
 ### BINDIR
-set -x PATH $PATH $BINARYDIR/sratoolkit.2.9.6-ubuntu64/bin $BINARYDIR/FastQC
+set -x PATH $PATH $BINARYDIR/sratoolkit.2.9.6-1-ubuntu64/bin $BINARYDIR/FastQC
 set -x PATH $PATH $BINARYDIR/hisat2-2.1.0/ $BINARYDIR/stringtie-1.3.5.Linux_x86_64 $BINARYDIR/gffcompare-0.10.6.Linux_x86_64/ $BINDIR/kallisto-current
 set -x PATH $PATH $BINARYDIR/userApps/bin
 
