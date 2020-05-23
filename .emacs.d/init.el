65b589f9f59aa9470754439e93f30f1304968e5b
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-05-23 21:37:40 +0900

    add helm-c-moccur

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,24 @@
 (setq x-select-enable-clipboard t)
 
 (require 'helm-config)
+(helm-descbinds-mode)
+(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+(package-install 'helm-c-moccur)
+(when (require 'helm-c-moccur nil t)
+  (setq
+   ;; 執筆時点でエラーが出たため定義しているが、
+   ;; 将来的には不要になる可能性が高い
+   helm-idle-delay 0.1
+   ;; helm-c-moccur用 `helm-idle-delay'
+   helm-c-moccur-helm-idle-delay 0.1
+   ;; バッファの情報をハイライトする
+   helm-c-moccur-higligt-info-line-flag t
+   ;; 現在選択中の候補の位置をほかのwindowに表示する
+   helm-c-moccur-enable-auto-look-flag t
+   ;; 起動時にポイントの位置の単語を初期パターンにする
+   helm-c-moccur-enable-initial-pattern t)
+  ;; C-M-oにhelm-c-moccur-occur-by-moccurを割り当てる
+  (global-set-key (kbd "C-M-o") 'helm-c-moccur-occur-by-moccur))
 
 (defun elisp-mode-hooks ()
   "lisp-mode-hooks"
