c18be779783096fd52c147219e280a0819c45f9c
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-06-19 10:13:11 +0900

    PATH

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -237,7 +237,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ricty Diminished" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Ricty" :foundry "PfEd" :slant normal :weight normal :height 121 :width normal)))))
 
  ;; 起動時のフレーム設定
 (setq initial-frame-alist
