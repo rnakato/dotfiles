221b1be9883365b73b97e8c33b4b1f65a5f2d42d
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2023-03-31 11:25:38 +0900

    modify config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,25 +3,25 @@ source $HOME/.config/fish/config.local.fish
 set -x BINARYDIR $GITDIR/binaries
 
 ### GITDIR
-set -x PATH $PATH $GITDIR/Cgaln $GITDIR/DROMPA3 $GITDIR/DROMPA3/scripts $GITDIR/DROMPAplus/scripts $GITDIR/DROMPAplus/bin $GITDIR/DROMPAplus/otherbins $GITDIR/DROMPAplus/submodules/cpdf/Linux-Intel-64bit $GITDIR/SSP/bin $GITDIR/ChIPseqTools/bin $GITDIR/script_rnakato $GITDIR/script_RNAseq $GITDIR/script_Hi-C
-set -x PATH $PATH $GITDIR/bedtools2/bin $GITDIR/bamtools/bin $GITDIR/salmon/bin $GITDIR/HOMER/bin $GITDIR/UCSC_utils/
+set -x PATH $PATH $GITDIR/DROMPAplus/scripts $GITDIR/DROMPAplus/bin $GITDIR/DROMPAplus/otherbins $GITDIR/DROMPAplus/submodules/cpdf/Linux-Intel-64bit $GITDIR/SSP/bin $GITDIR/ChIPseqTools/bin $GITDIR/script_rnakato $GITDIR/script_RNAseq $GITDIR/script_Hi-C
+set -x PATH $PATH $GITDIR/bedtools2/bin $GITDIR/bamtools/bin $GITDIR/HOMER/bin $GITDIR/UCSC_utils/
 set -x PATH $PATH $GITDIR/UCSC_utils
 
 ### BINDIR
 set -x PATH $PATH $BINARYDIR/RSEM-1.3.3 $BINARYDIR/STAR-2.7.3a/bin/Linux_x86_64_static $BINARYDIR/FastQC
 set -x PATH $PATH $BINARYDIR/gffcompare-0.10.6.Linux_x86_64/
-set -x PATH $PATH $BINARYDIR/userApps/bin
 set -x PATH $PATH $BINARYDIR/bedops_linux_x86_64-v2.4.39
 
+### Deprecated
+# set -x PATH $PATH $GITDIR/Cgaln $GITDIR/DROMPA3 $GITDIR/DROMPA3/scripts $GITDIR/salmon/bin 
+# set -x PATH $PATH $BINARYDIR/userApps/bin
+
 # GO
-set -x GOPATH $HOME/go
-set -x PATH /usr/local/go/bin $GOPATH/bin $PATH
+#set -x GOPATH $HOME/go
+#set -x PATH /usr/local/go/bin $GOPATH/bin $PATH
 
 # CellRanger
 set -x PATH $PATH /work/CellRanger/cellranger-current/ /work/CellRanger/cellranger-atac-current/
-
-### anaconda
-set -x PATH /work/anaconda3_202205/bin $PATH
 
 ### Emacs (Windows)
 set -x PATH $PATH /mnt/c/emacs-26.3-x86_64/bin
